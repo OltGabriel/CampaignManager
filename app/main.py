a0ff069533f7231d15e1418a57acbc6cb4492e26
og-tudor <tudor.frecus@gmail.com>
2025-06-24 16:04:01 +0300

    filler loop

--- app/main.py
+++ app/main.py
@@ -1,31 +1,43 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 from pathlib import Path
 import itertools
 
 app = FastAPI()
 
+print("LOADED MAIN")
+
+
 # Folder cu videoclipuri
-VIDEO_DIR = Path(__file__).parent.parent / "data" / "filler"
+VIDEO_DIR = Path(__file__).parent / "data" / "filler"
 
 
-# Creează un iterator infinit peste lista de videoclipuri
+# locatie HTML
+templates = Jinja2Templates(directory=str(Path(__file__).parent / "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+def video_player(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# loop infinit
 video_files = list(VIDEO_DIR.glob("*.mp4"))
-video_cycle = itertools.cycle(video_files)  # face loop infinit
+video_cycle = itertools.cycle(video_files)  
 
 
 @app.get("/next-video")
 def get_next_video():
-    print("Current working directory:", Path.cwd())
-    print("Looking for videos in:", VIDEO_DIR)
-    print("Exists VIDEO_DIR:", VIDEO_DIR.exists())
-    print("Files in VIDEO_DIR:", list(VIDEO_DIR.glob("*.mp4")))
+    global video_files, video_cycle
+    video_files = list(VIDEO_DIR.glob("*.mp4"))
     
+    if not video_files:
+        return {"error": "No video files found."}
+    
+    video_cycle = itertools.cycle(video_files)
     next_video = next(video_cycle)
-    print("Next video path:", next_video)
-    print("Exists:", next_video.exists())
     return FileResponse(path=next_video, media_type="video/mp4")
-
 
 # ruta /update POST
 # actualizeze JSON si sa descarce video-uri
