44cc21f16bfd9f11cffd35deca7185ca445bb14b
og-tudor <tudor.frecus@gmail.com>
2025-06-24 15:27:10 +0300

    second

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
-# Imagine de bază oficială Python
 FROM python:3.11-slim
 
-# Setează directorul de lucru în container
 WORKDIR /app
 
-# Copiază fișierele în container
 COPY app/ /app/
 
-# Instalează dependențele
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Expune portul 8000
 EXPOSE 8000
 
-# Comandă default de rulare
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.responses import FileResponse
+from pathlib import Path
+import itertools
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Dockerized FastAPI"}
+# Folder cu videoclipuri
+VIDEO_DIR = Path(__file__).parent.parent / "data" / "filler"
+
+
+# Creează un iterator infinit peste lista de videoclipuri
+video_files = list(VIDEO_DIR.glob("*.mp4"))
+video_cycle = itertools.cycle(video_files)  # face loop infinit
+
+
+@app.get("/next-video")
+def get_next_video():
+    print("Current working directory:", Path.cwd())
+    print("Looking for videos in:", VIDEO_DIR)
+    print("Exists VIDEO_DIR:", VIDEO_DIR.exists())
+    print("Files in VIDEO_DIR:", list(VIDEO_DIR.glob("*.mp4")))
+    
+    next_video = next(video_cycle)
+    print("Next video path:", next_video)
+    print("Exists:", next_video.exists())
+    return FileResponse(path=next_video, media_type="video/mp4")
+
+
+# ruta /update POST
+# actualizeze JSON si sa descarce video-uri
